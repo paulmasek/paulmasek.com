d53464358bd0736107b259d664315761ac34b172
Paul Masek <hello@paulmasek.com>
2017-01-31 15:35:52 +0000

    Added notify of assemble error

--- gulp/tasks/assemble.js
+++ gulp/tasks/assemble.js
@@ -3,6 +3,7 @@ import htmlmin from 'gulp-htmlmin'
 import extname from 'gulp-extname'
 import assemble from 'assemble'
 import gulpConfig from '../gulp-config'
+import gulpNotify from 'gulp-notify'
 
 const assembleApp = assemble()
 
@@ -17,6 +18,14 @@ gulp.task('assemble-load', (cb) => {
 gulp.task(gulpConfig.templates.task, ['assemble-load'], () => {
   return assembleApp.toStream('pages')
     .pipe(assembleApp.renderFile())
+    .on('error', function(err) {
+      gulpNotify({
+        title: 'Assemble render error',
+        message: err.message
+      }).write(err)
+      this.emit('end')
+      console.log(`Error: ${err.message}\nFile: ${err.path}`)
+    })
     .pipe(htmlmin())
     .pipe(extname())
     .pipe(assembleApp.dest(gulpConfig.templates.dest))
