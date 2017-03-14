c10052748370583b8c8ee267a9615d72876ecf8f
Paul Masek <hello@paulmasek.com>
2017-03-14 17:56:38 +0000

    Added check for el before setting up tween - avoids animation completly breaking if el is missing

--- src/assets/js/site-animation.js
+++ src/assets/js/site-animation.js
@@ -63,37 +63,41 @@ class SiteAnimation {
   }
 
   setupScene(config) {
-    const scene = new ScrollMagic.Scene(config.sceneOpts)
-    let tween = false
+    if (config.el) {
+      const scene = new ScrollMagic.Scene(config.sceneOpts)
+      let tween = false
 
-    switch (config.type) {
-      case 'class-toggle':
-        scene.setClassToggle(config.el, config.activeClass)
-        break
-      case 'to':
-        tween = TweenMax.to(config.el, 1, config.css)
-        break
-      case 'fromTo':
-        tween = TweenMax.fromTo(config.el, 1, config.from, config.to)
-        break
-      case 'staggerFromTo':
-        tween = TweenMax.staggerFromTo(config.el, 2, config.fromCSS, config.toCSS, config.staggerGap)
-        break
+      switch (config.type) {
+        case 'class-toggle':
+          scene.setClassToggle(config.el, config.activeClass)
+          break
+        case 'to':
+          tween = TweenMax.to(config.el, 1, config.css)
+          break
+        case 'fromTo':
+          tween = TweenMax.fromTo(config.el, 1, config.from, config.to)
+          break
+        case 'staggerFromTo':
+          tween = TweenMax.staggerFromTo(config.el, 2, config.fromCSS, config.toCSS, config.staggerGap)
+          break
+      }
+
+      if (tween && this.tweenOptions.includes(config.type)) {
+        scene.setTween(tween)
+      }
+
+      scene.addTo(this.controller)
+
+      if (this.debug || typeof config.debug !== 'undefined') {
+        scene.addIndicators({
+          name: config.name
+        })
+      }
+
+      this.scenes.push(scene)
+    } else {
+      console.log(`Could not set up scene ${config.name}, because el to animate could not be found`)
     }
-
-    if (tween && this.tweenOptions.includes(config.type)) {
-      scene.setTween(tween)
-    }
-
-    scene.addTo(this.controller)
-
-    if (this.debug || typeof config.debug !== 'undefined') {
-      scene.addIndicators({
-        name: config.name
-      })
-    }
-
-    this.scenes.push(scene)
   }
 
   setupScrollTo() {
