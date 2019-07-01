3b8ddf9e88d51cad47ed632c69dfd30efc179f9e
Paul Masek <hello@paulmasek.com>
2019-07-01 20:17:34 +0100

    Prettier and added simple section

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const NotFoundPage = () => {
+  return (
+    <Layout headerActive>
+      <SEO title="Page not found" />
+      <div className="simple-section container container--thin">
+        <h1 className="heading-three">Page not found</h1>
+        <p>You just hit a route that doesn't exist... the sadness.</p>
+      </div>
+    </Layout>
+  );
+};
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export default NotFoundPage
+export default NotFoundPage;
